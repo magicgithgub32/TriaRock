1f41156d4ba05cb653abbf4d9b53668cac142c70
Bea-ro <b_a_ro@hotmail.com>
2023-06-28 13:40:52 +0200

    add changes

--- src/api/routes/user-routes.js
+++ src/api/routes/user-routes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { getAllUsers, register, login, addOrRemoveFav, getUserByEmail } = require('../controllers/user-controllers');
 const router = express.Router();
 const { isAdmin } = require('../../middlewares/isAdmin-middleware');
+const { isAuth } = require('../../middlewares/isAuth-middleware');
 
 router.get('/', [isAdmin], getAllUsers);
 router.get('/:email', getUserByEmail);
 router.post('/register', register);
 router.post('/login', login);
-router.put('/:id/fav', addOrRemoveFav);
+router.put('/:id/fav', [isAuth], addOrRemoveFav);
 
 
 module.exports = router;
